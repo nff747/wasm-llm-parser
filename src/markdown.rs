047ebac1850292bce18819b60b971795fb644fc6
nff747 <nff747@users.noreply.github.com>
2026-09-24 23:36:23 -0300

    test: add test for strip_custom_tag_28

--- src/markdown.rs
+++ src/markdown.rs
@@ -187,6 +187,12 @@ mod tests {
     }
 
     #[test]
+    #[test]
+    fn test_strip_custom_tag_28() {
+        assert_eq!(strip_custom_tag_28("<tag28>text</tag28>"), "text");
+    }
+
+    #[test]
     fn test_italic_basic() {
         assert_eq!(parse_italic("*italic*"), "italic");
     }
