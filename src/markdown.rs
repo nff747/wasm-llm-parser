9311c2fe1c28612d6ece1935b34016fdb496d5bb
nff747 <nff747@users.noreply.github.com>
2026-09-24 23:36:23 -0300

    feat: stub strip_custom_tag_30

--- src/markdown.rs
+++ src/markdown.rs
@@ -337,3 +337,7 @@ pub fn strip_custom_tag_28(input: &str) -> String {
 pub fn strip_custom_tag_29(input: &str) -> String {
     input.replace("<tag29>", "").replace("</tag29>", "")
 }
+
+pub fn strip_custom_tag_30(input: &str) -> String {
+    String::new()
+}
