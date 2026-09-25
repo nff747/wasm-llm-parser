eac4d4ce00af5c337c42529cb2ebdb3802539cd0
nff747 <nff747@users.noreply.github.com>
2026-09-24 23:36:22 -0300

    test: add test for strip_custom_tag_9

--- src/markdown.rs
+++ src/markdown.rs
@@ -73,6 +73,12 @@ mod tests {
     }
 
     #[test]
+    #[test]
+    fn test_strip_custom_tag_9() {
+        assert_eq!(strip_custom_tag_9("<tag9>text</tag9>"), "text");
+    }
+
+    #[test]
     fn test_italic_basic() {
         assert_eq!(parse_italic("*italic*"), "italic");
     }
