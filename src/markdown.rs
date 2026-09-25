2ef3a1e7d9e23f514cb3f5081ff3c2ae2bdcf219
nff747 <nff747@users.noreply.github.com>
2026-09-24 23:36:23 -0300

    test: add test for strip_custom_tag_32

--- src/markdown.rs
+++ src/markdown.rs
@@ -211,6 +211,12 @@ mod tests {
     }
 
     #[test]
+    #[test]
+    fn test_strip_custom_tag_32() {
+        assert_eq!(strip_custom_tag_32("<tag32>text</tag32>"), "text");
+    }
+
+    #[test]
     fn test_italic_basic() {
         assert_eq!(parse_italic("*italic*"), "italic");
     }
